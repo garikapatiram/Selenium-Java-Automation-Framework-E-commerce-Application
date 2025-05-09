f303f7ad65cef9d25896e0f60dcfa3028b284bda
Rambabu <rambabu.garikapati@crunchyroll.com>
2025-05-09 22:32:38 +0530

    removerd unused imports

--- src/main/java/pages/CheckoutPage.java
+++ src/main/java/pages/CheckoutPage.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
